99afc0e01d4813a53f89487b8c7bec87178c6644
kinga <kinga@LAPTOP-7MTP1K5S>
2025-03-30 13:25:48 +0200

    branch a rotate commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             pictureBox1 = new PictureBox();
             button1 = new Button();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            button2 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -38,6 +42,7 @@
             pictureBox1.Location = new Point(292, 30);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(418, 344);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
@@ -51,22 +56,74 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(51, 26);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(57, 29);
+            radioButton1.TabIndex = 2;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "90";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(51, 61);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(67, 29);
+            radioButton2.TabIndex = 3;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "180";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(51, 96);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(67, 29);
+            radioButton3.TabIndex = 4;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "270";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(51, 131);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 5;
+            button2.Text = "Rotate";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(745, 414);
+            Controls.Add(button2);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
             Controls.Add(button1);
             Controls.Add(pictureBox1);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox pictureBox1;
         private Button button1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private Button button2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,8 +1,9 @@
-namespace lab4_wizualne
+﻿namespace lab4_wizualne
 {
     public partial class Form1 : Form
     {
         private Image image;
+        private int currentAngle = 0;
 
         public Form1()
         {
@@ -17,8 +18,52 @@ namespace lab4_wizualne
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 image = Image.FromFile(openFileDialog.FileName);
-                pictureBox1.Image = image; 
+                pictureBox1.Image = image;
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (image == null)
+            {
+                MessageBox.Show("Załaduj obrazek przed obrotem.");
+                return;
+            }
+
+            int angle = 0;
+
+            if (radioButton1.Checked)
+                angle = 90;
+            else if (radioButton2.Checked)
+                angle = 180;
+            else if (radioButton3.Checked)
+                angle = 270;
+
+            currentAngle = (currentAngle + angle) % 360;
+
+            pictureBox1.Image = RotateImage(image, currentAngle);
+        }
+
+        private Image RotateImage(Image image, float angle)
+        {
+            Bitmap bmp = new Bitmap(image); 
+
+            switch (angle)
+            {
+                case 90:
+                    bmp.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                    break;
+                case 180:
+                    bmp.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                    break;
+                case 270:
+                    bmp.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                    break;
+                default:
+                    break;
+            }
+
+            return bmp;
         }
     }
 }
